639de25b7d7685df43678dbbaefa4f72eaa2cc9c
Varun Tiwari <tiwarivarun819.vt33.vt@gmail.com>
2023-04-16 20:17:30 +0530

    Update nvm.sh

--- nvm.sh
+++ nvm.sh
@@ -4,10 +4,6 @@ export NVM_DIR="$HOME/.nvm" && (
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 )
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 export NVM_DIR="$HOME/.nvm" && (
   git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
   cd "$NVM_DIR"
